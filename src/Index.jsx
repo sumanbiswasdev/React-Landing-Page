bb3aa978eb7677daade1b5446431af1431d7fcff
sumanbrival <indianvloggersuman@gmail.com>
2026-07-03 15:51:36 -0300

    index.jsx was updateding

--- src/Index.jsx
+++ src/Index.jsx
@@ -86,7 +86,8 @@ const Index = () => {
                             <p className='text-white mb-5 md:text-lg'>Premium Standards</p>
                             <h1 className='font-bold mb-5 text-3xl md:text-[3.8rem] w-[450px] md:w-[600px] bg-gradient-to-r from-white via-gray-400 to-gray-500 bg-clip-text text-transparent'>Elevate Your Audio Experience with Sony Headphones</h1>
                             <p className='text-white mb-5 md:text-xl'>Elevate Your life with electronic designed for style and performance.</p>
-                            <button className='bg-white px-8 text-[18px] cursor-pointer py-3 text-black rounded-full md:mt-5 hover:bg-[#e8e8ef] transition-all ease-in-out duration-300'>Shop Now</button>
+                            <button className='bg-white px-8 text-[18px] cursor-pointer py-3 text-black 
+                            rounded-full md:mt-5 hover:bg-[#e8e8ef] transition-all ease-in-out duration-300'>Shop Now <span>&rarr;</span></button>
                         </div>
                         
                     </div>
@@ -97,37 +98,42 @@ const Index = () => {
             <section>
                 <div className='bg-white'>
                     <div className='container mx-auto'>
-                        <div className='py-5 md:flex justify-between items-center space-y-5 md:space-y-0 mx-5 md:mx-0'>
-                            <div className='relative'>
-
-                                <img className='w-full object-cover'
-                                src={bgimage5}
-                                alt="" />
-
-                                <div className='absolute inset-0'>
-                                    <p className='text-lg'>Unrivaled Precision</p>
-                                    <h1>Where Innovation Meets Immersive Sound</h1>
+                        <div className='grid grid-cols-1 md:grid-cols-2 md:gap-7 mx-5 md:mx-0'>
+                            {/* Right card */}
+                            <div className='bg-[#EEEEEE] mt-5 md:mt-0 relative rounded-2xl flex flex-col justify-between items-center overflow-hidden'>
+                                <div className='flex flex-col justify-between items-center absolute mt-8 md:mt-12 space-y-2 text-center'>
+                                    <p className='font-medium text-[16px] md:text-[18px]'>Unrivaled Precision</p>
+                                    <h1 className='font-bold text-3xl w-[350px] md:text-4xl md:w-[500px]'>Where Innovation Meets Immersive Sound</h1>
                                     <p>Power meets precision in every detail.</p>
-                                    <button>Shop Now</button>
-                                </div>
-                            </div>
-                            <div className='relative'>
-
-                                <div className='absolute inset-0'>
-                                    <p className='text-lg'>Unrivaled Precision</p>
-                                    <h1>Where Innovation Meets Immersive Sound</h1>
-                                    <p>Power meets precision in every detail.</p>
-                                    <button>Shop Now</button>
+                                    <button className='mt-2 bg-white px-8 text-[18px] cursor-pointer py-3 text-black rounded-full md:mt-5 hover:bg-[#e8e8ef] transition-all ease-in-out duration-300'>Shop Now <span>&rarr;</span></button>
                                 </div>
 
-                                <img className='w-full object-cover'
-                                src={bgimage6}
-                                alt="" />
-                            </div>
+                                <div className='md:w-full mt-24 md:mt-0'>
+                                    <img className='md:w-full md:h-auto md:object-cover' src={bgimage5} alt="" />
+                                </div>
 
-                            
+                            </div>
+                            {/* Left card */}
+                            <div className='bg-[#EEEEEE] mt-5 md:mt-0 relative rounded-2xl flex flex-col justify-between items-center overflow-hidden'>
+
+                                <div className='md:w-full mb-24 md:mb-0 md:mt-0'>
+                                    <img className='w-full h-auto object-cover' src={bgimage6} alt="" />
+                                </div>
+
+                                <div className='flex flex-col justify-between items-center absolute inset-be-0 mb-12 space-y-2 text-center'>
+                                    <p className='font-medium text-[16px] md:text-[18px]'>Peak Perfection</p>
+                                    <h1 className='font-bold text-3xl w-[340px] md:text-4xl md:w-[500px]'>Smart Solutions, Sleek Designs</h1>
+                                    <p>Smart solutions for a connected world.</p>
+                                    <button className='mt-2 bg-white px-8 text-[18px] cursor-pointer py-3 text-black 
+                                    rounded-full md:mt-5 hover:bg-[#e8e8ef] transition-all ease-in-out duration-300'>Shop Now <span>&rarr;</span></button>
+                                </div>
+
+                                
+
+                            </div>
 
                         </div>
+                        
                     </div>
                 </div>
             </section>

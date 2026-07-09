88be4bebed5ba26af4561d95ea97c8ce7a4af390
Suman Biswas <indianvloggersuman@gmail.com>
2026-07-09 20:43:28 -0300

    index.jsx design is ongoing

--- src/Index.jsx
+++ src/Index.jsx
@@ -80,7 +80,7 @@ const Index = () => {
 
             <section className='bg-white'>
                 <div className='container mx-auto relative'>
-                    <div className='py-5 mx-5 md:mx-0 overflow-hidden rounded-lg flex flex-col'>
+                    <div className='py-12 mx-5 md:mx-0 overflow-hidden rounded-lg flex flex-col'>
                         <img className='w-full h-[350px] md:h-full object-cover rounded-2xl'
                         src={bgimage4} 
                         alt="banner" />
@@ -100,7 +100,7 @@ const Index = () => {
             <section>
                 <div className='bg-white'>
                     <div className='container mx-auto'>
-                        <div className='grid grid-cols-1 md:grid-cols-2 md:gap-7 mx-5 md:mx-0'>
+                        <div className='grid grid-cols-1 md:grid-cols-2 md:gap-7 mx-5 md:mx-0 py-12'>
                             {/* Right card */}
                             <div className='bg-[#EEEEEE] mt-5 md:mt-0 relative rounded-2xl flex flex-col justify-between items-center overflow-hidden'>
                                 <div className='flex flex-col justify-between items-center absolute mt-8 md:mt-12 space-y-2 text-center'>
@@ -143,7 +143,7 @@ const Index = () => {
             <section>
                 <div className='bg-white'>
                     <div className='container mx-auto'>
-                        <div className='flex flex-col pt-16 pb-8'>
+                        <div className='flex flex-col py-12'>
 
                             <div className='md:flex items-center gap-8'>
                                 <h1 className='font-medium mx-5 md:mx-0 text-2xl md:text-5xl'>Capture Every Detail</h1>
@@ -152,22 +152,50 @@ const Index = () => {
 
                             <div className='mt-8 mx-auto md:mx-0 relative'>
 
-                                <video className='w-96 md:w-full md:h-auto rounded-2xl object-cover z-0' autoPlay muted loop playsInline>
+                                <video className='w-96 h-96 md:w-full md:h-auto rounded-2xl object-cover z-0' autoPlay muted loop playsInline>
                                     <source src={video1} type="" />
                                 </video>
 
                                 <div className='absolute inset-0 mx-auto md:mx-0 flex flex-col justify-center items-center
                                  text-white md:space-y-1 text-center w-[400px] md:w-full'>
                                     <p className='text-[15px] md:text-[18px] font-medium'>Highest Qualitys</p>
-                                    <h1 className='text-[25px] md:text-[4rem] md:w-[900px] font-bold'>Redifine Your Sound with Precision Microfones</h1>
-                                    <p className='text-[13px] w-[390px] md:text-[20px] text-gray-300 md:w-full font-light'>Innovative tech, timeless design, ultimate experience.</p> 
-                                    <button className='bg-white text-[18px] cursor-pointer p-2 mt-2 md:mt-0 md:px-8 md:py-3 text-black 
+                                    <h1 className='text-[25px] md:text-[4rem] md:w-[900px] font-bold mt-5 md:mt-0'>Redifine Your Sound with Precision Microfones</h1>
+                                    <p className='text-[15px] w-[310px] md:text-[20px] text-gray-300 md:w-full font-light mt-5 md:mt-0'>Innovative tech, timeless design, ultimate experience.</p> 
+                                    <button className='bg-white text-[18px] mt-5 md:mt-0 cursor-pointer p-2 mt-2 md:mt-0 md:px-8 md:py-3 text-black 
                                     rounded-full md:mt-5 hover:bg-[#e8e8ef] transition-all ease-in-out duration-300'>Shop Now <span>&rarr;</span></button>
 
                                 </div>
 
                             </div>
 
+                        </div>
+
+                    </div>
+
+                </div>
+            </section>
+
+            <section>
+                <div className='bg-[#F4F4F5]'>
+                    <div className='container mx-auto'>
+                        <div className='py-12'>
+                            
+                            <div className='text-center'>
+                                <h1>Our Happy Clients</h1>
+                                <p>See what our satisfied customers have to say about our electronic accessories.</p>
+                            </div>
+
+                            <div className='relative py-8'>
+                                <div className='absolute'>
+                                    <div className='grid grid-cols-1 md:grid-cols-4 gap-4 rounded-2xl p-5 bg-white'>
+
+                                    </div>
+                                    <button></button>
+                                </div>
+
+                            </div>
+
+                            <div></div>
                         </div>
 
                     </div>
